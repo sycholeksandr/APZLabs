2ecedad176ad0937029b0e185b35e970f02437a9
sycholeksandr <sychaleksand04@gmail.com>
2024-05-31 02:04:15 +0200

    lab5

---
+++ lab5APZ/api/mongo.js
@@ -0,0 +1,93 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const { MONGO_DB_HOSTNAME, MONGO_DB_PORT, MONGO_DB } = process.env;
+
+const url = `mongodb://${MONGO_DB_HOSTNAME}:${MONGO_DB_PORT}/${MONGO_DB}`;
+
+const userSchema = new Schema({
+  name: String,
+  age: Number,
+});
+
+const User = mongoose.model("User", userSchema);
+
+const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(url);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const closeMongoDB = async () => {
+  try {
+    await mongoose.connection.close();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const insertUser = async (user) => {
+  try {
+    const newUser = new User(user);
+    const savedUser = await newUser.save();
+    return savedUser;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getUser = async (id) => {
+  try {
+    return await User.findById(id);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getUsers = async () => {
+  try {
+    return await User.find({});
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const removeUserByName = async (id) => {
+  try {
+    await User.findByIdAndDelete(id);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const removeAllUsers = async () => {
+  try {
+    await User.deleteMany({});
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const updateUser = async (user) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(user._id, user, {
+      new: true,
+    });
+    return updatedUser;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+module.exports = {
+  connectMongoDB,
+  closeMongoDB,
+  insertUser,
+  getUser,
+  getUsers,
+  removeUserByName,
+  updateUser,
+  removeAllUsers,
+};
